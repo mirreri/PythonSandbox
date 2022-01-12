23a0c55c6797ec1c0e869a00276b3d106fa0e6e8
mirreri <mirreri@gmail.com>
2022-01-12 14:41:48 +0800

    Update package_recover.py

--- package_recover.py
+++ package_recover.py
@@ -1,4 +1,8 @@
 # if the sandbox remove the module path from module configuration but still keep the file in lib directory, can use this to recover it
-sys.modules['os'] = '/usr/lib/python3.8/os.py' # assume this is correct path for os.py
+# assume '/usr/lib/python3.8/os.py' is correct path for os.py
+# if it's not for your environment, can use "print (sys.modules['some basic module'])" to get the library path
+# for example "print (sys.modules['base64'])", or you can check on this website: https://docs.python.org/3/py-modindex.html
+print (sys.modules["base64"])
+sys.modules['os'] = '/usr/lib/python3.8/os.py'
 import os
 os.system ("ls /")
